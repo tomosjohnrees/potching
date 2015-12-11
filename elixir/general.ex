a8a1d4ba9959029259f0bc56ec0be67ba5c1caaf
Tomos John Rees <sabt@live.co.uk>
2015-12-11 21:54:31 +0000

    Refactor module to calculate the nth Fibonacci term

--- elixir/general.ex
+++ elixir/general.ex
@@ -4,17 +4,6 @@ defmodule Factorial do
 	def of(num), do: num * of(num - 1)
 end
 
-# Why doesn't this function for num > 23? Using too
-# much memory waiting to get to nth_term(0) and
-# nth_term(1) to begin calculations?
-defmodule Fibonacci do
-	def nth_term(0), do: 0
-
-	def nth_term(1), do: 1
-
-	def nth_term(num), do: nth_term(num - 2) + nth_term(num - 1)
-end
-
 defmodule Fibonacci do
 	def nth_term(0), do: 0
 
@@ -22,13 +11,11 @@ defmodule Fibonacci do
 
 	def nth_term(num), do: nth_term(num, [0,1])
 
-	def nth_term(1,terms) do
-		[ _, n2 ] = terms
+	def nth_term(1,[ _, n2 ]) do
 		n2
 	end
 
-	def nth_term(counter, terms) do
-		[ n1, n2 ] = terms
+	def nth_term(counter, [ n1, n2 ]) do
 		nth_term(counter - 1, [n2 , n1 + n2])
 	end
 end
