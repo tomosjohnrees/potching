9b3a5622f85e55b0445fc5092facd1ae897e2899
Tomos John Rees <sabt@live.co.uk>
2015-12-13 22:58:49 +0000

    Update general.ex

--- elixir/general.ex
+++ elixir/general.ex
@@ -6,13 +6,13 @@ end
 
 defmodule Fibonacci do
 	def nth_term(0), do: 0
-
+	
 	def nth_term(1), do: 1
-
+	
 	def nth_term(num), do: nth_term(num, [0,1])
-
+	
 	def nth_term(1,[ _, n2 ]), do: n2
-
+	
 	def nth_term(counter, [ n1, n2 ]) do
 		nth_term(counter - 1, [n2 , n1 + n2])
 	end
