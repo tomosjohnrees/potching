8885c8a3a561a452a178b8155e74946ab2fbd34b
Tomos John Rees <sabt@live.co.uk>
2015-12-19 20:21:07 +0000

    Distance between two points Kata and Greatest Prime Factor (Project Euler)

--- elixir/general.ex
+++ elixir/general.ex
@@ -17,3 +17,44 @@ defmodule Fibonacci do
     calculate(counter - 1, [n2 , n1 + n2])
   end
 end
+
+# Find the length between 2 co-ordinates. The 
+# co-ordinates are made of integers between -20
+#  and 20 and will be given in the form of a 2D 
+#  array:
+
+# (0,0) and (5,-7) would be [ [ 0 , 0 ] , [ 5, -7 ] ]
+
+defmodule Kata1 do
+  def distance_between_two_coordinates([[x1,y1],[x2,y2]]) do
+    :math.sqrt(:math.pow(x2 - x1,2) + :math.pow(y2 - y1,2))
+    |> round_to_two_decimal_places
+  end
+
+  defp round_to_two_decimal_places(num) do
+    Float.round(num,2)
+  end
+end
+
+#Project Euler
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+defmodule LargestPrimeFactor do
+  def of(num), do: of(num, 2, :math.sqrt(num) ,[])
+
+  def of(num, divisor, limit, []) when divisor > limit, do: num
+
+  def of(num, divisor, limit, [ largest_prime | _ ]) when divisor > limit do
+    largest_prime
+  end
+
+  def of(num, divisor, limit, result) when rem(num, divisor) == 0 do
+    of(round(num/divisor), divisor, limit, List.flatten([divisor, result]))
+  end
+
+  def of(num, divisor, limit, result) do
+    of(num, divisor + 1, limit, result)
+  end
+end
