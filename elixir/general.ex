8759b1181cb741deb8eb4e1cd67d6448e0dfa7a0
Tomos John Rees <sabt@live.co.uk>
2015-12-19 20:19:50 +0000

    Correct indentation

--- elixir/general.ex
+++ elixir/general.ex
@@ -1,20 +1,19 @@
 defmodule Factorial do
-	def of(0), do: 1
+  def of(0), do: 1
 
-	def of(num), do: num * of(num - 1)
+  def of(num), do: num * of(num - 1)
 end
 
 defmodule Fibonacci do
-	def nth_term(0), do: 0
-	
-	def nth_term(1), do: 1
-	
-	def nth_term(num), do: nth_term(num, [0,1])
-	
-	def nth_term(1,[ _, n2 ]), do: n2
-	
-	def nth_term(counter, [ n1, n2 ]) do
-		nth_term(counter - 1, [n2 , n1 + n2])
-	end
+  def nth_term(0), do: 0
+
+  def nth_term(1), do: 1
+
+  def nth_term(num), do: calculate(num, [0,1])
+
+  defp calculate(1,[ _, n2 ]), do: n2
+
+  defp calculate(counter, [ n1, n2 ]) do
+    calculate(counter - 1, [n2 , n1 + n2])
+  end
 end
- 
