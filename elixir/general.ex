a5a14b803be582012d48eb6a33b0c2e53172dd38
Tomos John Rees <sabt@live.co.uk>
2015-12-20 21:22:15 +0000

    Project Euler 6

--- elixir/general.ex
+++ elixir/general.ex
@@ -150,3 +150,24 @@ defmodule SumTheDigitsOfTheFactorial do
     Enum.map(list, fn(x) -> x |> String.to_integer end)
   end
 end
+
+# The sum of the squares of the first ten natural numbers is,
+
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
+
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+defmodule ProjectEuler6 do
+  def solution(lim) do
+    square_of_sum(lim) - sum_of_square(lim)
+  end
+
+  defp sum_of_square(lim), do: Enum.map(0..lim, fn(x) -> x * x end) |> Enum.sum
+
+  defp square_of_sum(lim), do: 0..lim |> Enum.sum |> square |> round
+
+  defp square(num), do: :math.pow(num, 2)
+end
